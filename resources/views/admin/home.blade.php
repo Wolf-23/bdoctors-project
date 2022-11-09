8774cfa83d9de8c04795d5ce7fe5f45a0b1aa26b
Riccardo <lupinu.ric@gmail.com>
2022-11-09 10:35:56 +0100

    Fix Home View

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container">
-        <a href="{{route('admin.profile')}}" class="btn btn-primary mb-3">Modifica Profilo</a>
+        <a href="{{route('admin.profile.edit', [Auth::user()->id])}}" class="btn btn-primary mb-3">Modifica Profilo</a>
         <h1>Benvenuto {{Auth::user()->name}} {{Auth::user()->surname}}</h1>
         <h3>Sei registrato con la mail {{Auth::user()->email}}</h3>
         <h4>Le tue Specializzazioni:
