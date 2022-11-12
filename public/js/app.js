aa799edd701e01dde2418a50c9940fde4e863ac9
ErikBorgogno <erikborgo.20@gmail.com>
2022-11-12 12:52:44 +0100

    Add Font Awesome

--- public/js/app.js
+++ public/js/app.js
@@ -49479,8 +49479,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/cavita/Documents/Final Project Boolean/bdoctors-project/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/cavita/Documents/Final Project Boolean/bdoctors-project/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\erikb\Desktop\BDoctors\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\erikb\Desktop\BDoctors\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- public/js/front.js
+++ public/js/front.js
@@ -18268,7 +18268,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/cavita/Documents/Final Project Boolean/bdoctors-project/resources/js/front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! C:\Users\erikb\Desktop\BDoctors\resources\js\front.js */"./resources/js/front.js");
 
 
 /***/ })
