1d6ea16763280eee5dcc7ced9792313664709a69
ErikBorgogno <erikborgo.20@gmail.com>
2022-11-15 23:08:46 +0100

    Add stars for reviews->vote

--- resources/views/admin/reviews/show.blade.php
+++ resources/views/admin/reviews/show.blade.php
@@ -3,7 +3,14 @@
 <div class="container">
     <a href="{{route('admin.reviews.index')}}" class="btn btn-primary mb-3">Torna alle recensioni</a>
     <h1>Hai ricevuto una recensione da: {{$review->name}} {{$review->surname}}</h1>
-    <h2>Voto ricevuto {{$review->vote}}</h2>
+    @for ($n = 1; $n <= 5; $n++)
+        @if ($review->vote < $n)
+            <i class="fa-star fa-regular"></i>
+        @else
+            <i class="fa-star fa-solid"></i> 
+        @endif
+    @endfor
+    
     <h3>Testo della recensione:</h3>
     <p> {{$review->review_text}}</p>
     <h5>Recensione ricevuta in data {{$review->created_at}}</h5>
