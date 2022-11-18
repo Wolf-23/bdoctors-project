a1b261053433173ca88a94fb63078aa2249884dc
ErikBorgogno <erikborgo.20@gmail.com>
2022-11-18 16:47:14 +0100

    Add validate and error, add message if status = true

--- resources/js/pages/MessageForm.vue
+++ resources/js/pages/MessageForm.vue
@@ -1,25 +1,46 @@
 <template>
   <div class="container-fluid eb_width">
-    <h1 class="pt-5 ml-3">Scrivimi</h1>
+    <h1 class="pt-5 text-center">Scrivimi</h1>
+    <div v-if="status" class="alert alert-success">
+        <i class="fa-solid  fa-circle-check"></i> Grazie di averci contattato! Il Dottore risponderà entro 48 ore!
+    </div>
     <form @submit.prevent='sendMessage'>
-      <div class="d-flex flex-column col-6">
-        <label for="name" class="mt-5 textGray">Nome</label>
-        <input type="text" v-model="name" name="name" id="name">
+      <div class="d-flex flex-column col-6 m-auto">
+        <label for="name" class="mt-5 textGray font-weight-bold">Nome</label>
+        <input type="text" :class="errors.name?'is-invalid':''" v-model="name" name="name" id="name">
+        <div v-for="(error, index) in errors.name" :key="index" class="invalid-feedback">
+            {{error}}
+        </div>
 
-        <label class="mt-3 textGray" for="surname">Cognome</label>
-        <input type="text" v-model="surname" name="surname" id="surname">
+        <label class="mt-3 textGray font-weight-bold" for="surname">Cognome</label>
+        <input type="text" :class="errors.surname?'is-invalid':''" v-model="surname" name="surname" id="surname">
+        <div v-for="(error, index) in errors.surname" :key="index" class="invalid-feedback">
+            {{error}}
+        </div>
 
-        <label class="mt-3 textGray" for="email">Email</label>
-        <input type="email" v-model="email" name="email" id="email">
+        <label class="mt-3 textGray font-weight-bold" for="email">Email</label>
+        <input type="email" :class="errors.email?'is-invalid':''" v-model="email" name="email" id="email">
+        <div v-for="(error, index) in errors.email" :key="index" class="invalid-feedback">
+            {{error}}
+        </div>
 
 
-        <label class="mt-3 textGray" for="title">Oggetto</label>
-        <input type="text" v-model="title" name="title" id="title">
+        <label class="mt-3 textGray font-weight-bold" for="title">Oggetto</label>
+        <input type="text" :class="errors.title?'is-invalid':''" v-model="title" name="title" id="title">
+        <div v-for="(error, index) in errors.title" :key="index" class="invalid-feedback">
+            {{error}}
+        </div>
 
-        <label class="mt-3 textGray" for="message_text">Messaggio</label>
-        <textarea v-model="messaggio" name="message_text"  id="message_text" rows="6"></textarea>
+        <label class="mt-3 textGray font-weight-bold" for="message_text">Messaggio</label>
+        <textarea v-model="messaggio" :class="errors.message_text?'is-invalid':''" name="message_text"  id="message_text" rows="6"></textarea>
+        <div v-for="(error, index) in errors.message_text" :key="index" class="invalid-feedback">
+            {{error}}
+        </div>
       </div>  
-        <button type="submit" :disabled="sending" class="btn btn-primary mt-3 ml-3 mb-5">Invia</button>
+      <div class="d-flex justify-content-center">
+        <button type="submit" :disabled="sending" class="btn btn-primary mt-3 p-2 mb-5">Invia</button>
+        <router-link :to="{name: 'single-profile'}" class="btn btn-primary mt-3 p-2 mb-5 ml-2">Torna al profilo</router-link>
+      </div>
     </form>
   </div>
 </template>
@@ -35,9 +56,10 @@ export default {
             email: '',
             title: '',
             messaggio: '',
-            status: '',
+            status: false,
             idProfile: '',
-            sending: false
+            sending: false,
+            errors: {}
 
         }
     },
@@ -61,12 +83,15 @@ export default {
                this.status = param.data.status
                this.sending = false
                if(this.status){
+                    this.errors = {};
                     this.name = '';
                     this.surname = '';
                     this.title = '';
                     this.email = '';
                     this.messaggio = '';
                     this.user_id = '';
+               }else{
+                  this.errors = param.data.errors;
                }
             })
         },
@@ -91,7 +116,6 @@ export default {
 
 <style scoped lang="scss">
    .eb_width{
-      height: calc(100vh - 71px);
       overflow-y: auto;
       input, textarea{
         border-radius: 10px;
