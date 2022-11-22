164e556796da50f002eece18611c988230fe9f12
Riccardo <lupinu.ric@gmail.com>
2022-11-22 23:43:11 +0100

    Fix: BackEnd View

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -22,45 +22,52 @@
         </div>
         <h1 class="text-center mt-2">Benvenuto Dr. {{Auth::user()->name}} {{Auth::user()->surname}}</h1>
         <span class="d-block text-center"><span class="font-weight-bold">Mail:</span> {{Auth::user()->email}}</span>
-        <div class="row d-flex align-items-center justify-content-center mt-2">
-            <div class="eb_square col-md-3 col-sm-6 mx-2 mb-2">
-                <h5 class="font-weight-bold text-center pt-2">Le Tue Specializzazioni</h5>
-                <ul class="d-flex flex-wrap px-1 justify-content-between pt-3">
+        <div class="row mt-3">
+            {{-- -------------- --}}
+            <div class="col-12 col-md-5 my-2 p-3 my-radius-bg">
+                <h5 class="font-weight-bold text-center">Le Tue Specializzazioni</h5>
+                <div class="d-flex flex-wrap">
                     @foreach (Auth::user()->specializations as $specialization)
-                        <li class="list-unstyled px-2">{{ $specialization->name }} </li>
+                        <span class="list-unstyled mr-3">{{ $specialization->name }} </span>
                     @endforeach
-                </ul>
+                </div>
             </div>
-            <div class="eb_square d-flex align-items-center justify-content-center mx-2 col-md-3 mb-2 col-sm-6">
-                @if ( Auth::user()->cv)
-                    <a class="btn eb_btn mt-3" href="{{ asset('storage/' .  Auth::user()->cv) }}" download>Download CV</a>
-                @else
-                    <h5 class="pt-2 font-weight-bold">Nessun CV caricato!</h5>  
-                @endif
-            </div>
-        </div>
-        <div class="row d-flex align-items-center justify-content-center">
-            <div class="eb_square col-md-3 col-sm-6 px-4 mt-2 mx-2">
+             {{-- -------------- --}}
+            <div class="col-12 col-md-5 my-2 my-radius-bg  offset-md-2 p-3">
                 @if (Auth::user()->services)
-                    <h5 class="text-center pt-2 font-weight-bold">Prestazioni offerte</h5>
+                    <h5 class="text-center font-weight-bold">Prestazioni offerte</h5>
                     <p>{{ Auth::user()->services }}</p>
                 @endif
             </div>
-            <div class="eb_square col-md-3 col-sm-6 mt-2 mx-2">
-                <div class="my-1 border-bottom text-center py-2">
-                    <h5>Hai ricevuto {{ count(Auth::user()->reviews) }} recensioni</h5>
+        </div>
+         {{-- -------------- --}}
+        <div class="row">
+            <div class="col-12 col-md-5 my-2 my-radius-bg  p-3">
+                <div class="text-center">
+                    <h5 class="mb-4">Hai ricevuto {{ count(Auth::user()->reviews) }} recensioni</h5>
                     <a href="{{route('admin.reviews.index', [Auth::user()->name])}}" class="btn eb_btn">Vedi Recensioni</a>
                 </div>
-                <div class="my-1 py-3 text-center">
-                    <h5>Hai ricevuto {{ count(Auth::user()->messages) }} messaggi</h5>
+            </div>
+            <div class="col-12 col-md-5 offset-md-2 my-2 my-radius-bg  p-3">
+                <div class="text-center">
+                    <h5 class="mb-4">Hai ricevuto {{ count(Auth::user()->messages) }} messaggi</h5>
                     <a href="{{route('admin.messages.index', [Auth::user()->name])}}" class="btn eb_btn">Vedi Messaggi</a>
                 </div>
             </div>
         </div>
+        <div class="row">
+            <div class="col-12 my-2 my-radius-bg p-3 text-center"> 
+                @if ( Auth::user()->cv)
+                <a class="btn eb_btn mt-3" href="{{ asset('storage/' .  Auth::user()->cv) }}" download>Download CV</a>
+                @else
+                <h5 class="pt-2 font-weight-bold">Nessun CV caricato!</h5>  
+                @endif
+            </div>
+        </div>
         <div class="mt-3 text-center">
-            <a href="{{route('admin.edit')}}" class="btn eb_btn">Modifica Profilo</a>
-            <a href="{{route('admin.sponsorship.index', [Auth::user()->name])}}" class="btn btn-success">Sponsorizza Profilo</a>
-           <form class="d-inline-block" action="{{route('admin.destroy')}}" method="POST">
+            <a href="{{route('admin.edit')}}" class="btn eb_btn mx-4">Modifica Profilo</a>
+            <a href="{{route('admin.sponsorship.index', [Auth::user()->name])}}" class="btn btn-success mx-4">Sponsorizza Profilo</a>
+           <form class="d-inline-block mx-4" action="{{route('admin.destroy')}}" method="POST">
                 @csrf
                 @method('DELETE')
                 <button type="submit" class="btn btn-danger">Elimina Profilo</button>
