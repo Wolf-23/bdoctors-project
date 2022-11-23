06d0c0b41c496fcc585a99d9c9b41090757b1613
pacimarco <m.paci1987@gmail.com>
2022-11-23 11:27:54 +0100

    cards pagamenti

--- public/js/front.js
+++ public/js/front.js
@@ -21625,7 +21625,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\erikb\Desktop\BDoctor\resources\js\front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! /Users/marcopaci/Desktop/bdoctors-project/resources/js/front.js */"./resources/js/front.js");
 
 
 /***/ })
